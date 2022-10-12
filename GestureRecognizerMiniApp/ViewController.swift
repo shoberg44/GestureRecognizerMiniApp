50485363be0e99682c30253b427f0fb77a5d8973
shoberg44 <steven.hoberg23@d155.org>
2022-10-12 08:11:31 -0500

    init commit

--- GestureRecognizerMiniApp/ViewController.swift
+++ GestureRecognizerMiniApp/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var moonImage: UIImageView!
+    @IBOutlet weak var labelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapScreenAction(_ sender: UITapGestureRecognizer) {
+        view.backgroundColor = UIColor.red
+        var tappedLoc = sender.location(in: view)
+        labelOutlet.center = tappedLoc
+    }
+    
+    @IBAction func moonPan(_ sender: UIPanGestureRecognizer) {
+        moonImage.center = sender.location(in: view)
+    }
 }
 
